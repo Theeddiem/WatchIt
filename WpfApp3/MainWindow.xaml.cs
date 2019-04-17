2b2f40cf86400b44974c58097d301cd6e47cccc7
Theeddiem <33320326+Theeddiem@users.noreply.github.com>
2019-04-17 16:52:31 +0300

    7

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Shapes;
 using System.IO;
-using Path = System.IO.Path;
+using Path = System.IO.Path;;
 using System.Collections.Generic;
 using System.Net;
 using OMDbApiNet;
