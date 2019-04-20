4b1fb164a3a9d6b84a74bc68bc07dc2fc121d3c8
Theeddiem <33320326+Theeddiem@users.noreply.github.com>
2019-04-20 19:26:56 +0300

    asd

--- ViewUI/Properties/AssemblyInfo.cs
+++ ViewUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApp3")]
+[assembly: AssemblyTitle("ViewUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WpfApp3")]
+[assembly: AssemblyProduct("ViewUI")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
