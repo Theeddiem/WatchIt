a45d14d74b760a4f491fac8cca3ac712752eae67
Theeddiem <33320326+Theeddiem@users.noreply.github.com>
2019-05-01 20:18:11 +0300

    asd

--- ViewUI/MainWindow.xaml.cs
+++ ViewUI/MainWindow.xaml.cs
@@ -33,6 +33,9 @@ namespace MainProgramUi
             getDataListBox.ItemsSource = m_ViewModelGlue.StoredFilesInPc;
             MoviesListBox.ItemsSource = m_ViewModelGlue.MoviesFound;
 
+            m_ViewModelGlue.sortType("By Rating");
+            SortTypeComboBox.SelectedIndex = 1;
+
         }   
 
         private void GetMoviesFromPc_Click(object sender, RoutedEventArgs e)
